0260be6f7e0a6d43fc45fbf6f3c96a37a332d32e
michelangelo.sparapano <michelangelo.sparapano@vodafone.com>
2025-04-22 16:19:22 +0200

    new control for binary search

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -2,6 +2,8 @@ def binary_search(my_list:list, target:int):
     low = 0
     high = len(my_list) -1 
     count = 0
+    if my_list != sorted(my_list):
+        return "Binary search works only with sorted list!"
     while low <= high:
         count += 1
         mid = (low + high) // 2
@@ -14,7 +16,8 @@ def binary_search(my_list:list, target:int):
             low = mid + 1
     return "Not Found: -1"
 
-print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 8))
-print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 54))
-print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 146], 79))
-print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 45))
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 8)) # test case n.1
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 54)) # test case n.2
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 146], 79)) # test case n.3
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 45)) # Element not present in the list
+print(binary_search([1, 144, 6, 8, 91, 4, 6, 54, 0, 12, 123, 145], 45)) # Not sorted List
