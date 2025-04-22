4cd5b89abb15e8d802b9123a305d1ca9d2793f82
michelangelo.sparapano <michelangelo.sparapano@vodafone.com>
2025-04-22 16:16:27 +0200

    add binary search algorithm

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(my_list:list, target:int):
+    low = 0
+    high = len(my_list) -1 
+    count = 0
+    while low <= high:
+        count += 1
+        mid = (low + high) // 2
+        guess = my_list[mid]
+        if guess == target:
+            return f"Element at index: {mid}, found after: {count} times"
+        elif guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return "Not Found: -1"
+
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 8))
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 54))
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 146], 79))
+print(binary_search([1, 4, 5, 6, 8, 9, 34, 54, 78, 79, 90, 102, 123, 145], 45))
