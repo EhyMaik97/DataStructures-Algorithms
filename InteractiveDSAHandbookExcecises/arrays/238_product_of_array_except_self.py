1d1e88d7fa1c56e4ef6359d417f74228b2ecf889
ehymaik97 <ehymaik97@gmail.com>
2025-06-30 21:18:56 +0200

    238_excercise

--- InteractiveDSAHandbookExcecises/arrays/238_product_of_array_except_self.py
+++ InteractiveDSAHandbookExcecises/arrays/238_product_of_array_except_self.py
@@ -4,4 +4,24 @@ https://leetcode.com/problems/product-of-array-except-self/
 
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        pass
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        n = len(nums)
+        answer = [1] * n
+        left = 1
+        for i in range(n):
+            answer[i] = left
+            left *= nums[i]
+
+        right = 1
+        for i in range(n -1, -1, -1):
+            answer[i] *= right 
+            right *= nums[i]
+        
+        return answer
+
+s = Solution()
+a = s.productExceptSelf([1,2,3,4])
+print(a)
