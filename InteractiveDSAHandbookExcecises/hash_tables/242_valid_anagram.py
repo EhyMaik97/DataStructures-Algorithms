fd142d0527716c55b0b207649adc17cb50bfad42
ehymaik97 <ehymaik97@gmail.com>
2025-08-25 21:29:06 +0200

    updated valid anagram problem using defaultdict

--- InteractiveDSAHandbookExcecises/hash_tables/242_valid_anagram.py
+++ InteractiveDSAHandbookExcecises/hash_tables/242_valid_anagram.py
@@ -1,6 +1,7 @@
 """
 https://leetcode.com/problems/valid-anagram/
 """
+from collections import defaultdict
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
@@ -17,8 +18,22 @@ class Solution:
         for i in t:
             t_maps[i] = t_maps.get(i, 0) + 1
         return s_maps == t_maps
-
     
+
+    def isAnagram_with_defaultdict(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_maps = defaultdict(int)
+        t_maps = defaultdict(int)
+
+        for char in s:
+            s_maps[char] += 1
+        for char in t:
+            t_maps[char] += 1
+
+        return s_maps == t_maps
+
 s = Solution()
 a = s.isAnagram("success", "sscceus")
 print(a)
